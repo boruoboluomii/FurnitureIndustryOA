3eb75b476ed2ecb5427c4083eb82b22f567ed6dc
shuo <443879838@qq.com>
2017-09-08 13:59:11 +0800

    WFTempController规范排版，添加一些js脚本，Temp的Add视图的更改再添加模板时对form表格进行验证，UserInfo的Index视图文件更改使弹出的dialog不产生多余的滚动条

--- Heima8.OA.UI.Portal/Controllers/WFTempController.cs
+++ Heima8.OA.UI.Portal/Controllers/WFTempController.cs
@@ -1,54 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Heima8.OA.IBLL;
 using Heima8.OA.Model;
 
 namespace Heima8.OA.UI.Portal.Controllers
 {
-
     public class WFTempController : Controller
     {
         //
         // GET: /WFTemp/
 
         public IWF_TempService WF_TempService { get; set; }
-        short delflagNormal = (short)Heima8.OA.Model.Enum.DelFlagEnum.Normal;
+        private short delflagNormal = (short) Heima8.OA.Model.Enum.DelFlagEnum.Normal;
+
         public ActionResult Index()
         {
             return View();
         }
+
         #region 加载数据
 
         public ActionResult GetAllTempInfos()
         {
-
             //easyui: table在初始化时候自动发送以下两个参数值。
             int pageSize = int.Parse(Request["rows"] ?? "10");
             int pageIndex = int.Parse(Request["page"] ?? "1");
             int total = 0;
 
             var temp = WF_TempService.GetPageEntities(pageSize, pageIndex, out total,
-                                              u => u.DelFlag == delflagNormal, u => u.ID, true);
+                u => u.DelFlag == delflagNormal, u => u.ID, true);
 
             var tempData =
                 temp.Select(
                     a =>
-                    new
-                    {
-                        a.ID,
-                        a.TepName,
-                        a.Remark,
-                        a.SubTime,
-                        a.ActityType,
-                        a.DelFlag
-                    });
+                        new
+                        {
+                            a.ID,
+                            a.TepName,
+                            a.Remark,
+                            a.SubTime,
+                            a.ActityType,
+                            a.DelFlag
+                        });
 
-            var data = new { total = total, rows = tempData.ToList() };
+            var data = new {total = total, rows = tempData.ToList()};
             return Json(data, JsonRequestBehavior.AllowGet);
-        } 
+        }
+
         #endregion
 
         #region 添加
@@ -69,19 +69,22 @@ namespace Heima8.OA.UI.Portal.Controllers
 
             return Content("ok");
         }
+
         #endregion
 
         #region 发起流程的index页面
+
         public ActionResult StartWF()
         {
-
             ViewData.Model = WF_TempService.GetEntities(u => u.DelFlag == DeleteFlag.DelflagNormal).ToList();
 
             return View();
         }
+
         #endregion
 
         #region 删除
+
         public ActionResult Delete(string ids)
         {
             if (string.IsNullOrEmpty(ids))
@@ -99,14 +102,16 @@ namespace Heima8.OA.UI.Portal.Controllers
             //UserInfoService.DeleteList(idList);
             WF_TempService.DeleteListByLogical(idList);
             return Content("ok");
-
         }
+
         #endregion
 
         #region 修改
+
         public ActionResult Edit(int id)
         {
-            ViewData.Model = WF_TempService.GetEntities(u => u.DelFlag == DeleteFlag.DelflagNormal && u.ID == id).FirstOrDefault();
+            ViewData.Model =
+                WF_TempService.GetEntities(u => u.DelFlag == DeleteFlag.DelflagNormal && u.ID == id).FirstOrDefault();
             return View();
         }
 
@@ -116,6 +121,7 @@ namespace Heima8.OA.UI.Portal.Controllers
             WF_TempService.Update(temp);
             return Content("ok");
         }
+
         #endregion
     }
 }
